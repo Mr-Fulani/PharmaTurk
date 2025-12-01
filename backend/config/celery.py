d6e2e0ecad29313048b314beb32d9101e6b47af7
Anvar <anvar.sharipov.1986@gmail.com>
2025-12-01 13:00:08 +0300

    fix: avoid early celery imports

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -9,13 +9,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("pharmaturk")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-# Автоматическое обнаружение задач во всех приложениях Django
 app.autodiscover_tasks()
-
-# Явно импортируем задачи из приложения orders для гарантированной регистрации
-try:
-    from apps.orders import tasks  # noqa: F401
-except ImportError:
-    pass
 
